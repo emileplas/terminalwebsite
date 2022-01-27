6029b52f32b8b6948f4e1066d96a7531f333ad16
emileplas <39679292+emileplas@users.noreply.github.com>
2022-01-27 11:26:28 +0100

    multiple messages working

--- js/introductionText.js
+++ js/introductionText.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded',function(event){
 
-    var introMessages = [ "I am a developer.", "This website is my portfolio website.", "blabla.", "another message"];
+    var introMessages = ["I am a developer.", "This website is my portfolio website.", "blabla.", "another message"];
 
     function insertHead(message, key){
         userClass = 'user'
         locationClass = 'location'
-        document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="firstmessage">${message}</span></p>`
+        document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="firstmessage"> ${message}</span></p>`
     }
 
-    for(i=0;i<introMessages.length;i++){
-        insertHead(introMessages[i], i)
+    var i = 0;               
+
+    function introLoop() {         
+        setTimeout(function() {   
+            insertHead(introMessages[i], i);   
+            i++;                  
+            if (i < introMessages.length) {           
+                introLoop();            
+            }                     
+        }, 3000)
     }
+
+    introLoop();
+
+    // for(i=0;i<introMessages.length;i++){
+    //     insertHead(introMessages[i], i)
+        
+    // }
     
 })
