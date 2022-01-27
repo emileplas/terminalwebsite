39b8661e7b707748ec4c433119d2dc0012ec0428
emileplas <39679292+emileplas@users.noreply.github.com>
2022-01-27 13:15:50 +0100

    typewriter working for one line

--- js/introductionText.js
+++ js/introductionText.js
@@ -2,25 +2,47 @@ document.addEventListener('DOMContentLoaded',function(event){
 
     var introMessages = ["I am a developer.", "This website is my portfolio website.", "blabla.", "another message"];
 
-    function insertHead(message, key){
+    var y=0;
+    
+    function typeWriter(message, key){
+        new Promise((resolve, reject) => {
+      
+        setTimeout(function() {   
+            document.getElementById( `messageSpan${key}`).innerHTML += message[y]
+            y++;                  
+            if (y < message.length) {           
+                typeWriter(message, key);            
+            }
+            else{
+                resolve()
+            }                     
+        }, 100)
+    })
+}
+    
+    async function insertHead(message, key){
+        new Promise((resolve, reject) => {
         userClass = 'user'
         locationClass = 'location'
-        document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="firstmessage"> ${message}</span></p>`
+        document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="messageSpan${key}"></span></p>`
+        resolve(typeWriter(message, key));
+    })
     }
 
     var i = 0;               
 
-    function introLoop() {         
-        setTimeout(function() {   
-            insertHead(introMessages[i], i);   
-            i++;                  
+    function introLoop(introMessages) {         
+        setTimeout(async function() {   
+            insertHead(introMessages[i], i)
+            i++
+                           
             if (i < introMessages.length) {           
-                introLoop();            
+                introLoop(introMessages);            
             }                     
         }, 1500)
     }
 
-    introLoop();
+    introLoop(introMessages);
 
     // for(i=0;i<introMessages.length;i++){
     //     insertHead(introMessages[i], i)
