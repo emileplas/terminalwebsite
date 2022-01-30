af581513e2e218af570a0357c3b65ba3efdfa231
emileplas <39679292+emileplas@users.noreply.github.com>
2022-01-30 16:47:35 +0100

    typewriter for one sentence working

--- js/introductionText.js
+++ js/introductionText.js
@@ -2,47 +2,98 @@ document.addEventListener('DOMContentLoaded',function(event){
 
     var introMessages = ["I am a developer.", "This website is my portfolio website.", "blabla.", "another message"];
 
-    var y=0;
-    
-    function typeWriter(message, key){
-        new Promise((resolve, reject) => {
-      
-        setTimeout(function() {   
-            document.getElementById( `messageSpan${key}`).innerHTML += message[y]
-            y++;                  
-            if (y < message.length) {           
-                typeWriter(message, key);            
+
+
+    function insertIntroCommand(message,key){
+        document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="messageSpan${key}"> </span></p>`
+        var i = 0;
+        var txt = message; /* The text */
+        var speed = 50; /* The speed/duration of the effect in milliseconds */      
+        var userClass = 'user'
+        var locationClass = 'location'
+        var id = `messageSpan${key}`
+
+        function typeWriter() {
+            if (i < txt.length) {
+                document.getElementById(id).innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
             }
-            else{
-                resolve()
-            }                     
-        }, 100)
-    })
-}
+        }
+        typeWriter()
+    }
+
+    insertIntroCommand(introMessages[0],1)
+
+   
     
-    async function insertHead(message, key){
-        new Promise((resolve, reject) => {
-        userClass = 'user'
-        locationClass = 'location'
-        document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="messageSpan${key}"></span></p>`
-        resolve(typeWriter(message, key));
-    })
-    }
+    // function typeWriter(message, key){
+    //     // new Promise((resolve, reject) => {
+    //         // message.forEach(letter => document.getElementById( `messageSpan${key}`).innerHTML += letter)
+    //             var message = message;
+    //             var speed = 50;
+    //             var userClass = 'user'
+    //             var locationClass = 'location'
+    //             document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="messageSpan${key}"></span></p>`
+          
+    //             if (y < message.length) {
+    //               document.getElementById(`messageSpan${key}`).innerHTML += message.charAt(y);
+    //               y++;
+    //               setTimeout(typeWriter, speed);
+    //             }
+    //             else if(y===message.length -1){
+    //                 resolve(true)
+    //             }
+    //           }
 
-    var i = 0;               
+    // typeWriter("I am a developer.", 1)
+      
+    //     setTimeout(function() {   
+    //         document.getElementById( `messageSpan${key}`).innerHTML += message[y]
+    //         y++;                  
+    //         if (y < message.length) {           
+    //             typeWriter(message, key);            
+    //         }
+    //         else{
+    //             resolve()
+    //         }                     
+    //     }, 100)
+    // })
 
-    function introLoop(introMessages) {         
-        setTimeout(async function() {   
-            insertHead(introMessages[i], i)
-            i++
+
+
+    
+    // async function insertHead(message, key){
+    //     new Promise((resolve, reject) => {
+    //         userClass = 'user'
+    //         locationClass = 'location'
+    //         document.getElementById("introMessage").innerHTML += `<p id='messageID${key}'><span class=${userClass}>administrator@Line-By-Line: </span><span class=${locationClass}>~ %</span><span class="message" id="messageSpan${key}"></span></p>`
+    //         var response = await typeWriter(message, key);
+    //         if(response === true){
+    //             resolve(true)
+    //         }
+    // })
+    // }
+
+    // var i = 0;               
+
+    // function introLoop(introMessages) {         
+    //     setTimeout(async function() {   
+    //         insertHead(introMessages[i], i)
+    //         .then((response){
+    //             if(response === true){
+    //                 i++
+    //             }
+    //         })
+         
                            
-            if (i < introMessages.length) {           
-                introLoop(introMessages);            
-            }                     
-        }, 1500)
-    }
+    //         if (i < introMessages.length) {           
+    //             introLoop(introMessages);            
+    //         }                     
+    //     }, 1500)
+    // }
 
-    introLoop(introMessages);
+    // introLoop(introMessages);
 
     // for(i=0;i<introMessages.length;i++){
     //     insertHead(introMessages[i], i)
