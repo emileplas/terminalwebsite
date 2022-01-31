c4dc67b3858bbf3603075f58621ef0c023b760c2
emileplas <39679292+emileplas@users.noreply.github.com>
2022-01-31 16:00:33 +0100

    changed for general insert function

--- js/validate.js
+++ js/validate.js
@@ -4,19 +4,49 @@ function darkMode() {
     element.classList.toggle("dark-mode");
  }
 
- Element.prototype.appendBefore = function (element) {
+ //functions to guarantee that element is inserted before or after
+Element.prototype.appendBefore = function (element) {
     element.parentNode.insertBefore(this, element);
   },false;
 
-  Element.prototype.appendAfter = function (element) {
+Element.prototype.appendAfter = function (element) {
     element.parentNode.insertBefore(this, element.nextSibling);
   },false;
 
- divArray = [];
+//array that holds an overview of all the divs that are present on the page
+divArray = [];
+
+//function that takes care of displaying and/or inserting new divs
+function insertElement(elementName){
+    var latestDiv = divArray[divArray.length -1]
+    if(divArray.length === 0){
+        // document.getElementById('input-form').prepend(document.getElementById('about'));
+        document.getElementById(elementName).style.display = 'block';
+        document.getElementById('input-form').value = '';
+        divArray.push(elementName)
+        console.log("We were in the first posstion")
+        console.log("This is the divArray: " + divArray)
+        
+    }
+    else if(divArray.length > 0 && divArray.includes(elementName)){
+        var copy = document.getElementById(elementName).cloneNode(true)
+        copy.appendAfter(document.getElementById(latestDiv));
+        document.getElementById('input-form').value = '';
+        console.log("We were in the second possition")
+        divArray.push(elementName)
+        
+    }else{
+        document.getElementById(elementName).appendAfter(document.getElementById(latestDiv));
+        document.getElementById(elementName).style.display = 'block';
+        document.getElementById('input-form').value = '';
+        console.log("We were in the third possition")
+        
+    }
+}
 
 function validate(){
  
-    var latestDiv = divArray[divArray.length -1]
+    
     let x = document.forms["inputForm"]["input"].value;
     switch (x) {
         case 'dark':
@@ -40,38 +70,50 @@ function validate(){
             darkMode();
             break;
         case 'about':
-            if(divArray.length === 0){
-                // document.getElementById('input-form').prepend(document.getElementById('about'));
-                document.getElementById('about').style.display = 'block';
-                document.getElementById('input-form').value = '';
-                divArray.push('about')
-                console.log("This is the divArray: " + divArray)
-                break;
-            }
-            else{
-                document.getElementById('about').appendAfter(document.getElementById(latestDiv));
-                document.getElementById('about').style.display = 'block';
-                document.getElementById('input-form').value = '';
-                break;
-            }
+            insertElement('about');
+            break;
+            // if(divArray.length === 0){
+            //     // document.getElementById('input-form').prepend(document.getElementById('about'));
+            //     document.getElementById('about').style.display = 'block';
+            //     document.getElementById('input-form').value = '';
+            //     divArray.push('about')
+            //     console.log("This is the divArray: " + divArray)
+            //     break;
+            // }
+            // else if(divArray.length > 0 && divArray.includes('about')){
+            //     var copy = document.getElementById('about').cloneNode(true)
+            //     copy.appendAfter(document.getElementById(latestDiv));
+            //     document.getElementById('input-form').value = '';
+            //     divArray.push('about')
+            //     break;
+            // }else{
+            //     document.getElementById('about').appendAfter(document.getElementById(latestDiv));
+            //     document.getElementById('about').style.display = 'block';
+            //     document.getElementById('input-form').value = '';
+            //     break;
+            // }
  
         case 'projects':
-            if(divArray.length === 0){
-                document.getElementById('projects').style.display = 'block';
-                document.getElementById('input-form').value = '';
-                divArray.push('projects')
-                console.log("This is the divArray: " + divArray)
-                break;
-            }else if(divArray.length > 0 && divArray.includes('projects')){
-                var copy = document.getElementById('projects').cloneNode(true)
-                copy.appendAfter(document.getElementById(latestDiv));
-                console.log("we are in the cloning zone. This is the divArray" + divArray)
-            }else{
-                document.getElementById('projects').appendAfter(document.getElementById(latestDiv));
-                document.getElementById('projects').style.display = 'block';
-                document.getElementById('input-form').value = '';
-                break
-            }
+            insertElement('projects')
+            break;
+            // if(divArray.length === 0){
+            //     document.getElementById('projects').style.display = 'block';
+            //     document.getElementById('input-form').value = '';
+            //     divArray.push('projects')
+            //     console.log("This is the divArray: " + divArray)
+            //     break;
+            // }else if(divArray.length > 0 && divArray.includes('projects')){
+            //     var copy = document.getElementById('projects').cloneNode(true)
+            //     copy.appendAfter(document.getElementById(latestDiv));
+            //     document.getElementById('input-form').value = '';
+            //     divArray.push('projects')
+            //     break;
+            // }else{
+            //     document.getElementById('projects').appendAfter(document.getElementById(latestDiv));
+            //     document.getElementById('projects').style.display = 'block';
+            //     document.getElementById('input-form').value = '';
+            //     break
+            // }
 
 
 
