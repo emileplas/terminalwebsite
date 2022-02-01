64de033d77ff67ca2859d5468519ec2eef1633f1
emileplas <39679292+emileplas@users.noreply.github.com>
2022-02-01 13:22:47 +0100

    cleaned up function

--- js/date.js
+++ js/date.js
@@ -3,7 +3,6 @@
 
 
 document.addEventListener('DOMContentLoaded',function getYear(){
-    console.log("the year function was called")
     var year = new Date().getFullYear();
     document.getElementById('year').innerHTML = `${year}`;
     
