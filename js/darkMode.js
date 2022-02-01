c58f6c9ce1d0abcf19b24a8c1a17a819796a349b
emileplas <39679292+emileplas@users.noreply.github.com>
2022-02-01 14:45:57 +0100

    created dark and light mode header

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,7 +1,36 @@
 function darkMode() {
     var body = document.body;
     var input = document.getElementById("input")
-    body.classList.toggle("dark-mode");
-    input.classList.toggle("dark-mode");
+    var user = document.getElementsByClassName('user')
+    var headerDark = document.getElementById('header-dark');
+    var headerLight = document.getElementById('header-light');
+
+    body.classList.toggle("light-mode");
+    input.classList.toggle("light-mode");
+
+
+
+
+    //makes sure that all elements from the user class are visible when light mode is switched on.
+    for(i=0;i<user.length;i++){
+        user[i].classList.toggle('user-light-mode')
+    }
+
+
+
+    //sets the header to a different style for better viewing purpuses when light mode is switched on. Changes back to dark header when light mode is switched off
+    if(headerDark.style.display === 'none'){
+        headerDark.style.display = 'block';
+        headerLight.style.display = 'none'
+    }
+    else{
+        headerLight.style.display = 'block';
+        headerDark.style.display = 'none';
+    }
+
+
+    
+    console.log(user);
+    
  }
 
